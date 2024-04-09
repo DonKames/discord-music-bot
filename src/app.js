895b72cceb676d310583d159d25d4d0c285bfe78
Dev Kames <dev.camilosantander@gmail.com>
2024-04-09 12:00:20 -0400

    Fix replayFromLastTime function to include player, link, and startTime parameters

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import {
 
 // Guarda el último tiempo de reproducción conocido
 let lastPlayTime = 0;
-
 // Indica si el bot está intentando reanudar la reproducción
 let isAttemptingReplay = false;
 
@@ -37,20 +36,20 @@ client.on('interactionCreate', async (interaction) => {
         await interaction.reply('Hello, World!');
     } else if (commandName === 'play') {
         const link = interaction.options.getString('link');
-        console.log('🚀 ~ client.on ~ link:', link);
 
         const videoInfo = await ytdl.getInfo(link);
-        // console.log('🚀 ~ client.on ~ videoInfo:', videoInfo.videoDetails);
+
         const videoTitle = videoInfo.videoDetails.title;
         const videoAuthor = videoInfo.videoDetails.author.name;
         const videoLength = videoInfo.videoDetails.lengthSeconds;
 
-        // Asegúrate de que el comando se usa en un servidor, no en DMs
+        // Revisa que el comando se usa en un servidor, no en DMs
         if (!interaction.guildId)
             return interaction.reply(
                 'Este comando solo puede ser usado en un servidor.',
             );
 
+        // Obtiene el miembro que ejecutó el comando
         const member = await interaction.guild.members.fetch(
             interaction.user.id,
         );
@@ -62,7 +61,7 @@ client.on('interactionCreate', async (interaction) => {
             );
         } else {
             try {
-                // Si el miembro está en un canal de voz, únete a ese canal
+                // Si el miembro está en un canal de voz, se une al canal
                 const connection = joinVoiceChannel({
                     channelId: member.voice.channelId,
                     guildId: interaction.guildId,
@@ -75,37 +74,26 @@ client.on('interactionCreate', async (interaction) => {
 
                 player.on('error', (error) => {
                     console.error('Error en el AudioPlayer:', error.message);
-                    // Implementa aquí cualquier lógica adicional para manejar el error
+
+                    // Intento de Reanudar la reproducción
                     if (!isAttemptingReplay) {
                         isAttemptingReplay = true;
-                        // Intenta reconectar después de un breve retraso
+                        // Intenta re-conectar después de un breve retraso
                         setTimeout(() => {
                             replayFromLastTime(player, link, lastPlayTime);
                         }, 1000); // Ajusta el tiempo de retraso según sea necesario
                     }
                 });
 
-                console.log('pasa createAudioPlayer');
-
                 connection.subscribe(player);
 
                 const stream = ytdl(link, { filter: 'audioonly' });
-                // console.log('🚀 ~ client.on ~ stream:', stream);
 
                 const resource = createAudioResource(stream);
 
-                console.log('pasa subscribe');
-
-                // const resource = createAudioResource('assets/Grabación.m4a', {
-                //     inputType: StreamType.Arbitrary,
-                // });
-
                 player.play(resource);
 
-                console.log('pasa createAudioResource');
-
                 await interaction.reply(`Reproduciendo: **${videoTitle}**`);
-                // await interaction.reply('Conectado!');
             } catch (error) {
                 console.log('error:', error);
                 await interaction.reply('Error al reproducir audio.');
