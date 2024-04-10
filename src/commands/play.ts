6fb741dd24b0c9584532495360a42a2b323eacb1
Dev Kames <dev.camilosantander@gmail.com>
2024-04-09 22:26:25 -0400

    Add YouTube song download functionality and command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -6,10 +6,16 @@ import {
     joinVoiceChannel,
 } from '@discordjs/voice';
 import { replayFromLastTime } from '../utils/voiceUtils';
+import fs from 'fs';
+import { promisify } from 'util';
+import { Readable } from 'stream';
+
+const writeFileAsync = promisify(fs.writeFile);
+const unlinkAsync = promisify(fs.unlink);
 
 const play: Command = {
     name: 'play',
-    description: 'Plays a song from YouTube',
+    description: 'Download and play a song from YouTube',
     execute: async (interaction) => {
         // Guarda el último tiempo de reproducción conocido
         let lastPlayTime = 0;
@@ -28,86 +34,95 @@ const play: Command = {
             await interaction.reply(
                 'Este comando solo puede ser usado en un servidor.',
             );
-
             return;
         }
 
-        const videoInfo = await ytdl.getInfo(link);
-        // console.log('🚀 ~ playCommand ~ videoInfo:', videoInfo);
-        const videoTitle = videoInfo.videoDetails.title;
-        console.log('🚀 ~ playCommand ~ videoTitle:', videoTitle);
+        // Define el nombre del archivo temporal
+        const tempFileName = `playing.mp4`;
 
-        // Revisa que el comando se usa en un servidor, no en DMs
-        if (!interaction.guildId) {
-            await interaction.reply(
-                'Este comando solo puede ser usado en un servidor.',
+        try {
+            // Obtiene información del video para el título
+            const videoInfo = await ytdl.getInfo(link);
+            const videoTitle = videoInfo.videoDetails.title;
+
+            // Descarga el video
+            const videoStream = ytdl(link, { filter: 'audioonly' });
+            const videoBuffer = await streamToBuffer(videoStream);
+            await writeFileAsync(tempFileName, videoBuffer as Buffer);
+
+            // Revisa que el comando se usa en un servidor, no en DMs
+            if (!interaction.guildId) {
+                await interaction.reply(
+                    'Este comando solo puede ser usado en un servidor.',
+                );
+                return;
+            }
+            // Revisa que exista guild
+            if (!interaction.guild) {
+                {
+                    await interaction.reply(
+                        'Este comando solo puede ser usado dentro de un servidor.',
+                    );
+                    return;
+                }
+            }
+
+            // Obtiene el miembro que ejecutó el comando
+            const member = await interaction.guild.members.fetch(
+                interaction.user.id,
             );
-            return;
-        }
-        // Revisa que exista guild
-        if (!interaction.guild) {
-            {
-                await interaction.reply(
-                    'Este comando solo puede ser usado dentro de un servidor.',
-                );
-                return;
+
+            // Verifica si el miembro está en un canal de voz
+            if (!member.voice.channelId) {
+                {
+                    await interaction.reply(
+                        'Debes estar en un canal de voz para usar este comando.',
+                    );
+                    return;
+                }
             }
-        }
 
-        // Obtiene el miembro que ejecutó el comando
-        const member = await interaction.guild.members.fetch(
-            interaction.user.id,
-        );
+            // Reproduce el archivo descargado
+            const connection = await joinVoiceChannel({
+                channelId: member.voice.channelId,
+                guildId: interaction.guildId,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
 
-        // Verifica si el miembro está en un canal de voz
-        if (!member.voice.channelId) {
-            {
-                await interaction.reply(
-                    'Debes estar en un canal de voz para usar este comando.',
-                );
-                return;
-            }
-        } else {
-            try {
-                // Si el miembro está en un canal de voz, se une al canal
-                const connection = joinVoiceChannel({
-                    channelId: member.voice.channelId,
-                    guildId: interaction.guildId,
-                    adapterCreator: interaction.guild.voiceAdapterCreator,
-                });
+            console.log(' al parecer se descargo y funco');
 
-                const player = createAudioPlayer();
+            const player = createAudioPlayer();
+            const resource = createAudioResource(tempFileName);
+            connection.subscribe(player);
+            player.play(resource);
 
-                player.on('error', (error) => {
-                    console.error('Error en el AudioPlayer:', error.message);
+            await interaction.reply(`Reproduciendo: **${videoTitle}**`);
 
-                    // Intento de Reanudar la reproducción
-                    if (!isAttemptingReplay) {
-                        isAttemptingReplay = true;
-                        // Intenta re-conectar después de un breve retraso
-                        setTimeout(() => {
-                            replayFromLastTime(player, link, lastPlayTime);
-                        }, 1000); // Ajusta el tiempo de retraso según sea necesario
-                    }
-                });
-
-                connection.subscribe(player);
-
-                const stream = ytdl(link, { filter: 'audioonly' });
-
-                const resource = createAudioResource(stream);
-
-                player.play(resource);
-
-                await interaction.reply(`Reproduciendo: **${videoTitle}**`);
-                return;
-            } catch (error) {
-                console.log('🚀 ~ playCommand ~ error:', error);
-                await interaction.reply('Error al reproducir audio.');
-                return;
-            }
+            // Elimina el archivo temporal después de la reproducción
+            player.on('stateChange', (oldState, newState) => {
+                if (newState.status === 'idle') {
+                    unlinkAsync(tempFileName).catch(console.error);
+                }
+            });
+        } catch (error) {
+            console.log('🚀 ~ execute: ~ error:', error);
+            await interaction.reply(
+                'Hubo un error al intentar reproducir el video.',
+            );
+            // Intenta eliminar el archivo temporal en caso de error
+            unlinkAsync(tempFileName).catch(console.error);
         }
     },
 };
 
 export default play;
+
+// Función auxiliar para convertir un stream a buffer
+function streamToBuffer(stream: Readable) {
+    return new Promise((resolve, reject) => {
+        const chunks: Buffer[] = [];
+        stream.on('data', (chunk: Buffer) => chunks.push(chunk));
+        stream.on('end', () => resolve(Buffer.concat(chunks)));
+        stream.on('error', reject);
+    });
+}
