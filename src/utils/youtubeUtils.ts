d88e3b7094ebf94ab8c90181a3cd23c0d0d965fd
DevCSF <dev.camilosantander@gmail.com>
2024-06-07 00:20:36 -0400

    youtubeUtils Refactored. Update npm dependencies and add googleapis package for YouTube search functionality

--- src/utils/youtubeUtils.ts
+++ src/utils/youtubeUtils.ts
@@ -1,43 +1,83 @@
 import "dotenv/config";
+import { google, youtube_v3 } from "googleapis";
 
 /**
  * The function `searchYouTube` asynchronously searches YouTube for videos based on a query and returns
- * information about the first result, or null if no results are found.
+ * an array of video objects containing video ID, title, and URL, or null if no results are found.
  * @param {string} query - The `query` parameter in the `searchYouTube` function is a string that
- * represents the search query for which you want to find videos on YouTube.
- * @returns The `searchYouTube` function returns a Promise that resolves to an object containing the
- * videoId, videoTitle, and videoUrl of the first search result from YouTube based on the provided
- * query. If no search results are found, it returns `null`.
+ * represents the search query for which you want to search YouTube videos. This query will be used to
+ * search for videos on YouTube using the YouTube Data API.
+ * @returns The `searchYouTube` function returns a Promise that resolves to an array of objects, where
+ * each object contains information about a YouTube video. The object structure includes `videoId`
+ * (string), `videoTitle` (string), and `videoUrl` (string). If there are no search results or an error
+ * occurs, the function returns `null`.
  */
 export async function searchYouTube(
   query: string
 ): Promise<{ videoId: string; videoTitle: string; videoUrl: string }[] | null> {
   try {
-    const searchUrl = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_TOKEN}&part=snippet&q=${encodeURIComponent(
-      query
-    )}&type=video`;
+    const youtube = google.youtube({
+      version: "v3",
+      auth: YOUTUBE_TOKEN,
+    });
 
-    const response = await fetch(searchUrl);
-    const searchData = await response.json();
+    const searchResponse = await youtube.search.list({
+      part: ["snippet"],
+      q: query,
+      type: ["video"],
+    });
 
-    const searchResults = searchData.items;
+    const searchResults = searchResponse.data.items;
 
-    if (!searchResults.length) {
+    if (!searchResults || searchResults.length === 0) {
       console.log("No se encontraron resultados para la búsqueda.");
       return null;
     }
 
-    const videos = searchResults.map((video: any) => ({
-      videoId: video.id.videoId,
-      videoTitle: video.snippet.title,
-      videoUrl: `https://www.youtube.com/watch?v=${video.id.videoId}`,
-    }));
+    const videos = searchResults.map(
+      (video: youtube_v3.Schema$SearchResult) => ({
+        videoId: video.id?.videoId || "",
+        videoTitle: video.snippet?.title || "",
+        videoUrl: `https://www.youtube.com/watch?v=${video.id?.videoId || ""}`,
+      })
+    );
 
     return videos;
   } catch (error) {
-    console.error("Error al buscar en YouTube:", error);
+    console.log("🚀 ~ error:", error);
     return null;
   }
 }
+
+// export async function searchYouTube(
+//   query: string
+// ): Promise<{ videoId: string; videoTitle: string; videoUrl: string }[] | null> {
+//   try {
+//     const searchUrl = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_TOKEN}&part=snippet&q=${encodeURIComponent(
+//       query
+//     )}&type=video`;
+
+//     const response = await fetch(searchUrl);
+//     const searchData = await response.json();
+
+//     const searchResults = searchData.items;
+
+//     if (!searchResults.length) {
+//       console.log("No se encontraron resultados para la búsqueda.");
+//       return null;
+//     }
+
+//     const videos = searchResults.map((video: any) => ({
+//       videoId: video.id.videoId,
+//       videoTitle: video.snippet.title,
+//       videoUrl: `https://www.youtube.com/watch?v=${video.id.videoId}`,
+//     }));
+
+//     return videos;
+//   } catch (error) {
+//     console.error("Error al buscar en YouTube:", error);
+//     return null;
+//   }
+// }
 
 const YOUTUBE_TOKEN = process.env.YOUTUBE_KEY;
